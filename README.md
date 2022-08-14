57ef2886bc0931844919425f92a6cec083a7d5b2
Andrea Fang <arhughes43@gmail.com>
2022-08-14 11:16:35 -0700

    readme

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # Budget Tracker
 
-An app that tracks expenses. 
-Designed for offline functionality. 
+An app that tracks expenses. Designed for offline functionality. 
+
+## Description
+Enter a name and amount to change your total. Negative numbers are not necessary - use the 'Subtract Funds' button for expenses.
+When you add or remove funds, your new total is displayed at the top and your history is updated in the table and graph below. <br>
+If you enter changes with poor or no internet connection, your updates will be saved via IndexedDB and added when a connection is reestablished.
+
+## Screenshot 
+![Screenshot](public/budget-tracker.png)
+
+## Technologies
+This app uses a MongoDB database, and the Mongoose object modeling tool. IndexedDB is used to achieve offline functionality.<br>
+
+## Live Site
+A demo version of this application is deployed to [Heroku](https://budget-tracker-c19.herokuapp.com/).
--- public/service-worker.js
+++ public/service-worker.js
@@ -12,16 +12,14 @@ self.addEventListener('fetch', function (e) {
     console.log('fetch request : ' + e.request.url)
     e.respondWith(
       caches.match(e.request).then(function (request) {
-        if (request) { // if cache is available, respond with cache
+        if (request) { 
           console.log('responding with cache : ' + e.request.url)
           return request
-        } else {       // if there are no cache, try fetching request
+        } else { 
           console.log('file is not cached, fetching : ' + e.request.url)
           return fetch(e.request)
         }
-  
-        // You can omit if/else for console.log & put one line below like this too.
-        // return request || fetch(e.request)
+
       })
     )
   })
@@ -35,7 +33,7 @@ self.addEventListener('install', function(e) {
     )
 })
 
-// delete old cache
+// delete the old cache
 self.addEventListener('activate', function (e) {
     e.waitUntil(
         caches.keys().then(function (keyList) {
@@ -55,4 +53,3 @@ self.addEventListener('activate', function (e) {
         }) 
     );
 });
-
